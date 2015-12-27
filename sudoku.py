0057679ccf00511e8e8bedd1fafc094b5df0d764
cdrouadaine <clement.drouadaine@orange.fr>
2015-12-27 19:12:35 +0100

    Now kinda working. No error in code, although algorithm might be significantly improved

--- sudoku.py
+++ sudoku.py
@@ -1,15 +1,16 @@
 import itertools
+from time import sleep
 import numpy as np
 
 numbers = '123456789'
 subsquares = [0,3,6]
 #inequalities, line by line. 0 for <, 1 for >
-ineq = [[[0,1,0,0,0,1],[1,0,0,1,1,1],[0,1,1,1,0,1],[1,1,1,1,0,1],[1,0,0,1,0,1],[1,1,0,0,1,0],[1,0,1,0,1,0],[1,1,0,1,1,0],[0,1,0,1,0,0]],
-[[1,0,1,0,0,1],[1,0,0,1,1,0],[0,1,0,1,1,1],[1,1,0,0,1,1],[0,1,1,1,0,1],[0,0,1,1,0,1],[0,1,0,0,1,0],[1,0,1,0,1,0],[1,1,0,1,1,1]]]
-#second line is column by column, grid turned CLOCKWISE
+ineqHori = [[0,1,0,0,0,1],[1,0,0,1,1,1],[0,1,1,1,0,1],[1,1,1,1,0,1],[1,0,0,1,0,1],[1,1,0,0,1,0],[1,0,1,0,1,0],[1,1,0,1,1,0],[0,1,0,1,0,0]]
+#line by line, 0 for ^, 1 for v
+ineqVert = [[1,1,0,0,0,1,0,1,1],[1,0,1,0,1,1,1,0,1],[0,1,0,1,1,0,0,0,1],[1,0,0,1,1,0,1,1,0],[1,1,1,0,0,1,1,1,0],[1,0,0,1,1,1,1,0,1]]
 
 def randomGrid():
-	grid = np.array([[0 for x in range(9)] for x in range(9)])
+	grid = [[0 for x in range(9)] for x in range(9)]
 	for firstRow in subsquares:
 		for firstCol in subsquares:
 			square = ''.join(np.random.permutation(list(numbers)))
@@ -18,12 +19,12 @@ def randomGrid():
 				for col in [x + firstCol for x in range(3)]:
 					grid[row][col] = square[i]
 					i += 1
-	return grid
+	return np.array(grid)
 
 def grid2string(grid):
 	gridString = ''
-	for row in range(9):
-		for col in range(9):
+	for row in range(len(grid)):
+		for col in range(len(grid[row])):
 			gridString += str(grid[row][col])
 		gridString += '\n'
 	return gridString
@@ -46,10 +47,10 @@ def checkSquares(grid):
 	return True
 
 def checkValid(grid):
-	lines = checkLines(npgrid)
+	lines = checkLines(grid)
 	if lines != 10:
 		return False
-	squares = checkSquares(npgrid)
+	squares = checkSquares(grid)
 	if squares != 10:
 		return False
 	return True
@@ -57,6 +58,7 @@ def checkValid(grid):
 	
 def swapNumbers():
 		
+	retries = 0
 	while(True):
 		grid = randomGrid()
 		swapped = 1
@@ -66,35 +68,40 @@ def swapNumbers():
 		
 		while(swapped > 0):
 			swapped = 0
+
+			for row in range(9):
+				i = 0
+				for col in [0,1,3,4,6,7]:
+					if (grid[row][col] > grid[row][col+1]) != ineqHori[row][i]:
+						swapped += 1
+						tmp = grid[row][col]
+						grid[row][col] = grid[row][col+1]
+						grid[row][col+1] = tmp
+					i += 1
 			
-			for transposed in range(2):
-				for row in range(9):
-					i = 0
-					for col in [0,1,3,4,6,7]:
-						if (grid[row][col] > grid[row][col+1]) != ineq[transposed][row][i]:
-							swapped += 1
-							tmp = grid[row][col]
-							grid[row][col] = grid[row][col+1]
-							grid[row][col+1] = tmp
-						i += 1
-					
-				grid = grid.transpose()
+			i = 0
+			for row in [0,1,3,4,6,7]:
+				for col in range(9):
+					if (grid[row][col] > grid[row+1][col]) != ineqVert[i][col]:
+						swapped += 1
+						tmp = grid[row][col]
+						grid[row][col] = grid[row+1][col]
+						grid[row+1][col] = tmp		
+				i += 1			
 						
 			iterations += 1
 			totalSwaps += swapped
-			print('-----')
-			print('Iterations: '+str(iterations))
-			print('Swaps: '+str(swapped))
+			#print('-----')
+			#print('Iterations: '+str(iterations))
+			#print('Swaps: '+str(swapped))
 		
-		print('-------------------')
-		print('-------------------')
-		print('-------------------')
-		print('Total iterations: '+str(iterations))
-		print('Total swaps: '+str(totalSwaps))
+		print('Retries: '+str(retries)+', Iterations: '+str(iterations)+', Total swaps: '+str(totalSwaps))
+		retries += 1
 
 		if(checkValid(grid)):
 			print(grid2string(grid))
 			return 0
-	
 
+#print(grid2string(randomGrid()))
 swapNumbers()
+#print(checkLines(randomGrid()))
